1346aa7c69a2f416a93b3ca0be7a78454aa225d8
rrbyn <robin.kukk23@gmail.com>
2021-03-09 08:43:47 +0200

    bruh

--- src/Lesson1.php
+++ src/Lesson1.php
@@ -6,17 +6,22 @@ class Lesson1
 {
     public function helloWorld(){
         /// Kirjuta oma kood siia
-
+        return "Hello, World!";
     }
 
     public function createArray()
     {
         /// Kirjuta oma kood siia
-
+        $array = [
+            "key1" => "item1",
+            "key2" => "item2",
+            "key3" => "item3",
+        ];
+        return $array;
     }
 
     public function addFunction($a,$b){
         /// Kirjuta oma kood siia
-
+        return ($a + $b);
     }
 }
