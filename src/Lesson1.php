c465b86ee1a31bf61a6c40bec075132a6b17e8aa
kevinrull1 <kevin.rull@ametikool.ee>
2021-03-09 09:07:11 +0200

    x

--- src/Lesson1.php
+++ src/Lesson1.php
@@ -5,18 +5,18 @@ namespace Lessons;
 class Lesson1
 {
     public function helloWorld(){
-        /// Kirjuta oma kood siia
-
+        return "Hello, World!";
     }
 
     public function createArray()
     {
-        /// Kirjuta oma kood siia
+       $array=[1,2,3];
+       return $array;
 
     }
 
     public function addFunction($a,$b){
-        /// Kirjuta oma kood siia
+        return $a + $b;
 
     }
 }
