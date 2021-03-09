5b18eab749a3f968a766416b8e2dcd0e322c3338
karljame <karl321jame@gmail.com>
2021-03-09 10:26:43 +0200

    done

--- src/Lesson1.php
+++ src/Lesson1.php
@@ -6,17 +6,24 @@ class Lesson1
 {
     public function helloWorld(){
         /// Kirjuta oma kood siia
-
+        return "Hello, World!";
     }
 
     public function createArray()
     {
-        /// Kirjuta oma kood siia
-
+        /// Kirjuta oma kood siia#
+    $array = [
+        "mhm1" => "jah1",
+        "mhm2" => "jah2",
+        "mhm3" => "jah3",
+    ];
+      return $array;  
     }
 
-    public function addFunction($a,$b){
+    public function addFunction( int $a, int $b){
         /// Kirjuta oma kood siia
-
+        return $a +$b;
     }
 }
+
+?>
