e4c16051d58086f58786560fcad91305b895354e
Rando Õispuu <rando@innofly.eu>
2021-03-19 08:05:19 +0200

    Add Lesson2 with tests

---
+++ src/Lesson2.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Lessons;
+
+
+class Lesson2
+{
+    public function calcPercentageOf($a, $b)
+    {
+        //Kirjuta kood siia
+
+    }
+
+    public function calcArray(array $array)
+    {
+        //Kirjuta oma kood siia
+
+    }
+}
---
+++ tests/Lesson2Test.php
@@ -0,0 +1,15 @@
+<?php
+
+use Lessons\Lesson2;
+
+it('returns the percentage of the number in correct format', function (){
+    expect((new Lesson2())->calcPercentageOf(100, 20))->toBe('20.00 %');
+    expect((new Lesson2())->calcPercentageOf(995, 60))->toBe('6.03 %');
+    expect((new Lesson2())->calcPercentageOf(9, 23.4))->toBe('260.00 %');
+    expect((new Lesson2())->calcPercentageOf(109, 34))->toBe('31.19 %');
+});
+
+it( 'calculates the contents of an array', function () {
+    $array = [3,6,27,8,4,6,7,54,3,5,6,7,65,4,1,23,12,767,31,64,42,54,94];
+    expect((new Lesson2())->calcArray($array))->toBe(94);
+});
